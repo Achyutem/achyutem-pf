a80f1953bdcc035ba44336a1e0fc7c533d3f2c52
achyutem <achyutem.ad@gmail.com>
2025-04-24 20:04:50 +0530

    removed github config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,8 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
-import github from "@astrojs/github";
 
 export default defineConfig({
-	site: "https://achyutem.github.io/",
-	output: "static",
-	integrations: [mdx(), sitemap(), tailwind(), github()],
+	site: "https://achyutem.vercel.app",
+	integrations: [mdx(), sitemap(), tailwind()],
 });
