51dbbdcaa13869dfe5f8a9369663334b5b995a3e
achyutem <achyutem.ad@gmail.com>
2025-04-24 18:06:51 +0530

    updated metadata

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
+import github from "@astrojs/github";
 
-// https://astro.build/config
 export default defineConfig({
-  site: 'https://astrofy-template.netlify.app',
-  integrations: [mdx(), sitemap(), tailwind()]
+	site: "https://achyutem.github.io/",
+	output: "static",
+	integrations: [mdx(), sitemap(), tailwind(), github()],
 });
